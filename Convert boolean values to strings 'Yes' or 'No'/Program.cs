4a25336f43882aad50c3cf63f3603bdc5f1fd0e4
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 11:13:47 +0200

    Add project files.

---
+++ Convert boolean values to strings 'Yes' or 'No'/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Convert_boolean_values_to_strings__Yes__or__No__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static string boolToWord(bool word)
+        {
+            if (word == true)          
+                return "Yes";
+            
+            else          
+                return "No";
+            
+        }
+    }
+}
